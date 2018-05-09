ffb3e2685f3190a780427aa675dcaecd88712e3f
jimfede <jimfede@gmail.com>
2018-05-08 22:41:11 -0300

    completado
    se hace solucion mia con if white if not white
    se hace solucion de colt con boolean siBlue
    se hace otra solucion de colt con toggle que fue mi primera idea pero no supe como hacerla

---
+++ changeBckCol/main.js
@@ -0,0 +1,16 @@
+var btn = document.querySelector("button");
+// var isBlue = false;
+// btn.addEventListener("click", toggleColor);
+
+// function toggleColor() {
+//     if (isBlue) {
+//         document.body.style.background = "white"
+//     }
+//     else {
+//         document.body.style.background = "blue  ";
+//     }
+//     isBlue = !isBlue;
+// }
+btn.addEventListener("click", function () {
+    document.body.classList.toggle("blue");
+});
