128484a0a9199ffb112d1cc8334bb0e19d870b90
jimfede <jimfede@gmail.com>
2018-05-10 13:19:05 -0300

    segun colt
    como lo hizo colt

--- scoreKeeper/main.js
+++ scoreKeeper/main.js
@@ -4,11 +4,20 @@ var p1Display = document.querySelector("#p1Display");;
 var p2Display = document.querySelector("#p2Display");;
 var puntosP1 = 0;
 var puntosP2 = 0;
+var juegoTerminado = false;
+var puntajeGanador = 5;
+
 
 btnP1.addEventListener("click", function () {
-    puntosP1++;
-    p1Display.textContent = puntosP1;
+    if (!juegoTerminado) {
+        puntosP1++;
+        if (puntosP1 === puntajeGanador) {
+            console.log("Juego terminado")
+        }
+    }
+    p1Display.textContent = puntosP1
 });
+
 
 btnP2.addEventListener("click", function () {
     puntosP2++;
