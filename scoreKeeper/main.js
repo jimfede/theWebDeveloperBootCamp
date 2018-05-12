638899c890033244dcb78a730b52a7f6f355185d
jimfede <jimfede@gmail.com>
2018-05-12 19:23:06 -0300

    terminado
    scorekeeper corregido y funcionando

--- scoreKeeper/main.js
+++ scoreKeeper/main.js
@@ -1,25 +1,53 @@
 var btnP1 = document.querySelector("#p1");
 var btnP2 = document.getElementById("p2");
-var p1Display = document.querySelector("#p1Display");;
-var p2Display = document.querySelector("#p2Display");;
+var p1Display = document.querySelector("#p1Display");
+var p2Display = document.querySelector("#p2Display");
+var btnReset = document.getElementById("res");
+var txtPuntajeGanador = document.querySelector("input");
+var puntajeGanadorDisplay = document.getElementById("puntajeGanadorDisplay");
 var puntosP1 = 0;
 var puntosP2 = 0;
 var juegoTerminado = false;
-var puntajeGanador = 5;
+var puntajeGanador = 0;
 
+txtPuntajeGanador.addEventListener("change", function () {
+    puntajeGanadorDisplay.textContent = this.value;
+    puntajeGanador = Number(this.value);
+    reset();
+});
+
+btnReset.addEventListener("click", function () {
+    reset();
+});
 
 btnP1.addEventListener("click", function () {
     if (!juegoTerminado) {
         puntosP1++;
         if (puntosP1 === puntajeGanador) {
-            console.log("Juego terminado")
+            p1Display.classList.add("winner");
+            juegoTerminado = true;
         }
     }
     p1Display.textContent = puntosP1
 });
 
-
 btnP2.addEventListener("click", function () {
-    puntosP2++;
+    if (!juegoTerminado) {
+        puntosP2++;
+        if (puntosP2 === puntajeGanador) {
+            p2Display.classList.add("winner");
+            juegoTerminado = true;
+        }
+    }
     p2Display.textContent = puntosP2;
 });
+
+function reset() {
+    puntosP1 = 0;
+    puntosP2 = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
+    juegoTerminado = false;
+}
