e083416c263d1d0ae4dab262ffbcdfdf260b3845
jimfede <jimfede@gmail.com>
2018-05-15 13:00:29 -0300

    juego completo
    todo funcionando

---
+++ colorGame/colorGame.js
@@ -0,0 +1,62 @@
+var colors = generateRandomColors(6);
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.getElementById("message");
+
+colorDisplay.textContent = pickedColor;
+
+for (var i = 0; i < squares.length; i++) {
+    //se agregan colores iniciales
+    squares[i].style.backgroundColor = colors[i];
+    //se agrega click listener a los cuadrados
+    squares[i].addEventListener("click", function () {
+        //tomar color del cuadrado seleccionado
+        var clickedColor = this.style.backgroundColor;
+        //comparar el color elegido
+        if (clickedColor === pickedColor) {
+            messageDisplay.textContent = "Correcto!";
+            changeColors(pickedColor);
+        }
+        else {
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Intente nuevamente";
+        }
+    });
+
+}
+
+function changeColors(color) {
+    //loop por todos los colores
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
+
+function generateRandomColors(num) {
+    //hacer un arreglo
+    var arr = [];
+    //repetir num veces
+    for (var i = 0; i < num; i++) {
+        //obtengo un color randomico y lo agrego al arreglo
+        arr.push(randomColor());
+    }
+    //retornar el arreglo
+    return arr;
+}
+
+function randomColor() {
+    //elegir un "red" de 0 - 255
+    var r = Math.floor(Math.random() * 256);
+    //elegir un "green" de 0 - 255
+    var g = Math.floor(Math.random() * 256);
+    //elegir un "blue" de 0 - 255
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+}
