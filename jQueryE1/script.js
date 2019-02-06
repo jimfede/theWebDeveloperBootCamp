6fa7ca2bc2f1477ef3a1eb3a8605cdf54592fd12
jimfede <jimfede@gmail.com>
2019-02-06 15:56:20 -0300

    ejercicio terminado
    sin corrección

---
+++ jQueryE1/script.js
@@ -0,0 +1,14 @@
+$("div").css("background","purple");
+$(".highlight").css("width","200px");
+$("#third").css({
+    "border-color": "orange",
+    "border-width": "2px",
+    "border-style": "solid"
+});
+$("body div:first-child").css("color","pink");
+
+
+// var divs = document.querySelectorAll("div");
+// for(i = 0; i < divs.length; i++) {
+//   divs[i].style.color = "yellow";
+// }
